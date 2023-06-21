59f52ea77b9907307887ed182bea0a21ec8840ad
Camila Bogarin <camzz154@gmail.com>
2023-06-21 11:54:18 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'parcial3camilabogarin',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
